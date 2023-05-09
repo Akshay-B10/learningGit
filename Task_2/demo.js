8a893d78b4c0af13727e880515c0fe34d8071c0f
Akshay-B10 <akshaybhokare777@gmail.com>
2023-05-09 14:40:58 +0530

    Task_2 has index.html and demo.js file

---
+++ Task_2/demo.js
@@ -0,0 +1,5 @@
+const header = document.getElementById("main-header");
+header.style.borderBottom = "solid 3px black";
+const formTitle = document.getElementById("main").firstElementChild;
+formTitle.innerHTML = "<b>Add Items</b>";
+formTitle.style.color = "green";
