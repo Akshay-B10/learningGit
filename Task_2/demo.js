fbd7d9f95d40993b0c2e12c97f5afac4dd800422
Akshay-B10 <akshaybhokare777@gmail.com>
2023-05-09 15:46:32 +0530

    changed background; made text in list item bold

--- Task_2/demo.js
+++ Task_2/demo.js
@@ -1,5 +1,15 @@
+/*
 const header = document.getElementById("main-header");
 header.style.borderBottom = "solid 3px black";
 const formTitle = document.getElementById("main").firstElementChild;
 formTitle.innerHTML = "<b>Add Items</b>";
 formTitle.style.color = "green";
+*/
+console.log("Hello World")
+const liGrp = document.getElementsByClassName("list-group-item");
+console.log(liGrp);
+liGrp[2].style.backgroundColor = "green";
+for (let i = 0; i < liGrp.length; i++)
+{
+    liGrp[i].style.fontWeight = "bold";
+}
