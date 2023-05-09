7d37199a8ddc1e40e3dcd47d523d5881462e023e
Akshay-B10 <akshaybhokare777@gmail.com>
2023-05-09 16:13:53 +0530

    added li tag with class name as "list-not-same"

--- Task_2/demo.js
+++ Task_2/demo.js
@@ -5,11 +5,17 @@ const formTitle = document.getElementById("main").firstElementChild;
 formTitle.innerHTML = "<b>Add Items</b>";
 formTitle.style.color = "green";
 */
-console.log("Hello World")
+// console.log("Hello World")
 const liGrp = document.getElementsByClassName("list-group-item");
-console.log(liGrp);
 liGrp[2].style.backgroundColor = "green";
 for (let i = 0; i < liGrp.length; i++)
 {
     liGrp[i].style.fontWeight = "bold";
 }
+const lastLi = document.getElementsByClassName("list-not-same");
+
+// lastLi is array of Node elements even if there is only one element; Imp to remember
+
+const li = document.getElementsByTagName("li");
+lastLi[0].style.backgroundColor = "purple";
+li[4].style.color = "white";
