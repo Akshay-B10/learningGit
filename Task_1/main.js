2e012a46498413ba566b78eb63e919ef17e2adf3
Akshay-B10 <akshaybhokare777@gmail.com>
2023-05-11 16:37:54 +0530

    Displays user details

--- Task_1/main.js
+++ Task_1/main.js
@@ -1,35 +1,21 @@
-/*
-function getCall(event){
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    console.log(name);
-    console.log(document.getElementById("email").value);
-    console.log(document.getElementById("phone").value);
-    console.log(document.getElementById("date").value);
-    console.log(document.getElementById("time").value);
-}
-*/
-/*
-const ul = document.getElementsByTagName("ul")
-ul.firstElementChild.style.color = "red"
-ul.children[1].style.color = "green"
-*/
+const containerDiv = document.querySelector(".container");
+const ul = document.createElement("ul");
+ul.className = "list-group";
+containerDiv.appendChild(ul);
 
-// const myForm = document.querySelector("form");
 const btn = document.querySelector(".btn");
-btn.addEventListener("click", e =>{
+btn.addEventListener("click", e => {
     e.preventDefault();
     const name = document.querySelector("#name");
     const email = document.querySelector("#email");
     const phone = document.querySelector("#phone");
     const date = document.querySelector("#date");
     const time = document.querySelector("#time");
-    if (name.value == "" || email.value == "" || phone.value == "" || date.value == "" || time.value == "")
-    {
+    if (name.value == "" || email.value == "" || phone.value == "" || date.value == "" || time.value == "") {
         alert("Please fill required details");
-    } else
-    {
-        document.querySelector(".btn").style.background = "red";
+    } else {
+        // Storing data in local storage
+        document.querySelector(".btn").className = "btn btn-success";
         const userDetails = {};
         userDetails.Name = name.value;
         userDetails.Email = email.value;
@@ -37,14 +23,12 @@ btn.addEventListener("click", e =>{
         userDetails.Date = date.value;
         userDetails.Time = time.value;
         localStorage.setItem(email.value, JSON.stringify(userDetails));
+
+        // Displaying users
+        const li = document.createElement("li");
+        li.className = "list-group-item";
+        li.appendChild(document.createTextNode(`Name: ${name.value}, Email: ${email.value}, Phone: ${phone.value}, Date: ${date.value}, Time: ${time.value}`));
+        ul.appendChild(li);
     }
 
 })
-btn.addEventListener("mouseover", e =>{
-    // e.preventDefault();
-    document.querySelector(".btn").style.color = "black";
-});
-btn.addEventListener("mouseout", e =>{
-    // e.preventDefault();
-    document.querySelector(".btn").style.color = "white";
-});
