f855e7db800056a1e60fc0ce6e4a3a59301ba24b
Akshay-B10 <akshaybhokare777@gmail.com>
2023-06-19 20:00:35 +0530

    create, update, delete post using async and await.

--- Task_5/promiseAll.js
+++ Task_5/promiseAll.js
@@ -1,88 +1,51 @@
 const posts = [];
 var srNo = 1;
 
-function logPosts() {
+function logPost() {
     console.log(posts);
 }
 
-function updateLastUserActivity() {
-    return new Promise((resolve, reject) => {
+async function createAndUpdatePost() {
+    const createPost = await new Promise((res, rej) => {
         setTimeout(() => {
-            posts[posts.length - 1].lastActivity = new Date().getTime();
-            console.log(posts[posts.length - 1]);
-            resolve();
-        }, 1000);
-    });
-}
-
-function createPost() {
-    return new Promise((resolve, reject) => {
-        // Math.floor((Math.random() * 10) + 1) -> use to get random integer from 1 to 10
-        setTimeout(() => {
-            // let postNo = Math.floor((Math.random() * 100) + 1);
-            let postNo = window.srNo;
+            posts.push({ title: `Post ${window.srNo}` });
             window.srNo++;
-            posts.push({title: `Post ${postNo}`});
-            resolve(updateLastUserActivity);
+            res();
         }, 2000);
     });
-}
 
-function delPost() {
-    return new Promise((resolve, reject) => {
+    const updatePost = await new Promise((res, rej) => {
         setTimeout(() => {
-            if (posts.length > 0)
-            {
-                posts.pop()
-                resolve(logPosts);
-            } else
-            {
-                reject("Error: Could not delete post");
-            }
+            posts[posts.length - 1].lastActivity = new Date().getTime();
+            res();
         }, 1000);
     });
+    console.log(posts[posts.length - 1]);
 }
-/*
-Promise.all([updateLastUserActivity, createPost]).then((func) => { func() })
-.catch((err) => {
-    console.log(err);
-});
-Promise.all([updateLastUserActivity, createPost]).then((func) => { func() })
-.catch((err) => {
-    console.log(err);
-});
-Promise.all([updateLastUserActivity, createPost]).then((func) => { func() })
-.catch((err) => {
-    console.log(err);
-});
-Promise.all([delPost]).then((func) => { func() })
-.catch((err) => {
-    console.log(err);
-});
-Promise.all([updateLastUserActivity, createPost]).then((func) => { func() })
-.catch((err) => {
-    console.log(err);
-})
-Promise.all([delPost]).then((func) => { func() })
-.catch((err) => {
-    console.log(err);
-});
-*/
-createPost().then((func) => {
-    func().then(() => {
-        createPost().then((func) => {
-            func().then(() => {
-                createPost().then((func) => {
-                    func().then(() => {
-                        delPost().then((func) => {
-                            func();
-                            delPost().then((func) => {
-                                func();
-                            }).catch((err) => console.log(err))
-                        }).catch((err) => console.log(err))
-                    })
-                })
-            })
-        })
-    })
-})
+
+async function delPost() {
+    try{
+        const deletePost = await new Promise((res, rej) => {
+            setTimeout(() => {
+                if (posts.length > 0) {
+                    posts.pop();
+                    res();
+                } else {
+                    rej("Error: Could not delete post")
+                }
+            }, 1000)
+        });
+        logPost();
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+async function abc() {
+    await createAndUpdatePost();
+    await createAndUpdatePost();
+    await delPost();
+    await delPost();
+    await delPost();
+}
+abc();
