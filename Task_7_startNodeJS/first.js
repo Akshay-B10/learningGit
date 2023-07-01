72f4e8084557afc8dbc797159099f8802a86f80b
Akshay-B10 <akshaybhokare777@gmail.com>
2023-07-01 20:32:12 +0530

    async function and promise.

--- Task_7_startNodeJS/first.js
+++ Task_7_startNodeJS/first.js
@@ -1,15 +1,18 @@
-const obj = {
-    key1: 10,
-    key2: 20,
-    key3: 30
-};
-let {key1, key3} = {...obj};
-console.log(key1, key3); // 10 30
-
-const arr = ["val1", "val2"];
-const [v1, v2] = arr;
-console.log(v1, v2); // val1 val2
-
-key1 = 1;
-key2 = 2;
-console.log(obj.key1, obj.key3); // 10 30
+async function x() {
+    console.log('a');
+    console.log('b');
+    const prom1 = await new Promise((res) => {
+        setTimeout(() => {
+            console.log('c');
+            res();
+        }, 3000);
+    });
+    const prom2 = await new Promise((res) => {
+        setTimeout(() => {
+            console.log('d');
+            res();
+        }, 0);
+    });
+    console.log('e');
+}
+x();
