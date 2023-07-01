ab34b8fdd6b1cdc3801eca77632dbbe4760eb369
Akshay-B10 <akshaybhokare777@gmail.com>
2023-07-01 19:03:43 +0530

    use case of map.

--- Task_7_startNodeJS/first.js
+++ Task_7_startNodeJS/first.js
@@ -1,4 +1,10 @@
-const multiplyTwoNum = (x, y) => {
-    return x * y;
-}
-console.log(multiplyTwoNum(4, 7));
+const arr = ["apple", "oranges", " ", "mango", " ", "lemon"];
+const newArr = arr.map((fruit) => {
+    if (fruit == " ") {
+        return "empty string";
+    } else {
+        return fruit;
+    }
+})
+console.log(newArr);
+// Output: ["apple", "oranges", "empty string", "mango", "empty string", "lemon"]
