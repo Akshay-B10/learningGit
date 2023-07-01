516c113ddd2a38569e005b3f592c3a68706f53a3
Akshay-B10 <akshaybhokare777@gmail.com>
2023-07-01 19:25:36 +0530

    understanding destructing.

--- Task_7_startNodeJS/first.js
+++ Task_7_startNodeJS/first.js
@@ -1,10 +1,15 @@
-const arr = ["apple", "oranges", " ", "mango", " ", "lemon"];
-const newArr = arr.map((fruit) => {
-    if (fruit == " ") {
-        return "empty string";
-    } else {
-        return fruit;
-    }
-})
-console.log(newArr);
-// Output: ["apple", "oranges", "empty string", "mango", "empty string", "lemon"]
+const obj = {
+    key1: 10,
+    key2: 20,
+    key3: 30
+};
+let {key1, key3} = {...obj};
+console.log(key1, key3); // 10 30
+
+const arr = ["val1", "val2"];
+const [v1, v2] = arr;
+console.log(v1, v2); // val1 val2
+
+key1 = 1;
+key2 = 2;
+console.log(obj.key1, obj.key3); // 10 30
