4addcc8062a5be3b96c0e62fb5be0b51df863759
Akshay-B10 <akshaybhokare777@gmail.com>
2023-07-03 20:43:38 +0530

    Routes and Filter; added admin.js, shop.js

--- Task_7_expressJS/app.js
+++ Task_7_expressJS/app.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use("/add-product", (req, res, next) => {
-    res.send("<form action = '/product' method ='POST'><input type = 'text' name = 'title'><input type = 'text' name = 'size'><button type = 'submit'>Add Product</button></form>")
-});
+app.use("/admin", adminRoutes);
 
-app.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-});
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.send("<h1>Hello to NodeJS</h1>");
+    res.status(404).send("<h1>Page not Found.</h1>");
 });
 
 app.listen(4000);
