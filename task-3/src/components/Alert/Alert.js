e08590b892df35cf9a38f915f8186634d9af3a4b
Akshay-B10 <akshaybhokare777@gmail.com>
2023-10-16 19:55:44 +0530

    Fragments, React portal

--- task-3/src/components/Alert/Alert.js
+++ task-3/src/components/Alert/Alert.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 
 import Card from "../UI/Card";
 import "./Alert.css";
+
+const Backdrop = () => {
+    return <div className="backdrop" />
+}
+
+const ModalOverlay = props => {
+    return (
+        <Card className="modal">
+            <header className="header">
+                <h2>Invalid Input</h2>
+            </header>
+            <div className="content">
+                <p>{props.message}</p>
+            </div>
+            <footer className="actions">
+                <button type="button" className="modal_btn" onClick={props.onClick}>Cancel</button>
+            </footer>
+        </Card>
+    );
+}
 
 const Alert = props => {
     const clickHandler = () => {
@@ -9,20 +30,16 @@ const Alert = props => {
     }
 
     return (
-        <div>
-            <div className="backdrop" />
-            <Card className="modal">
-                <header className="header">
-                    <h2>Invalid Input</h2>
-                </header>
-                <div className="content">
-                    <p>{props.message}</p>
-                </div>
-                <footer className="actions">
-                    <button type="button" className="modal_btn" onClick={clickHandler}>Cancel</button>
-                </footer>
-            </Card>
-        </div>
+        <Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop />,
+                document.getElementById("modal_backdrop")
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay message={props.message} onClick={clickHandler} />,
+                document.getElementById("modal_overlay")
+            )}
+        </Fragment>
     )
 };
 
